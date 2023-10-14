58a3d107aa2575d542fa2f7acffd47fdf51fd00c
Cedric Ong <cedricong99@gmail.com>
2023-10-14 22:18:14 +0800

    temp commit

--- src/e2e/java/teammates/e2e/cases/playwright/BaseE2ETest.java
+++ src/e2e/java/teammates/e2e/cases/playwright/BaseE2ETest.java
@@ -49,7 +49,7 @@ public abstract class BaseE2ETest extends BaseTestCaseWithDatabaseAccess {
   @BeforeClass
   void launchBrowser() {
     playwright = Playwright.create();
-    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
   }
 
   @AfterClass
---
+++ src/e2e/java/teammates/e2e/cases/playwright/pages/InstructorHomePage.java
@@ -0,0 +1,10 @@
+package teammates.e2e.cases.playwright.pages;
+
+import com.microsoft.playwright.Page;
+
+public class InstructorHomePage extends BasePage {
+  
+    public InstructorHomePage(Page page) {
+        super(page);
+    }
+}
